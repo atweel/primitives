e16cdac9a518de69ea9c78ba96aac53bb23f1cd1
Eduard Malakhov <eduard@atweel.com>
2020-03-30 07:53:10 +0000

    Clean up jest config

--- jest.config.js
+++ jest.config.js
@@ -3,12 +3,9 @@ const { pathsToModuleNameMapper } = require('ts-jest/utils');
 // which contains the path mapping (ie the `compilerOptions.paths` option):
 const { compilerOptions } = require('./tsconfig');
 
-const path = require('path');
-
 module.exports = {
     globalSetup: './tests/hooks/setup.js',
     globalTeardown: './tests/hooks/teardown.js',
-    // resolver: './tests/hooks/resolve.js',
     preset: 'ts-jest',
     moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
         prefix: __dirname + '/'
